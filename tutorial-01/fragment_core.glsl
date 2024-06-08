98ad4b74fce6f07267d6d8135a52b0ada42dc5da
Sonu Lohani <sonulohani@gmail.com>
2024-06-08 17:06:40 +0530

    Added diffuse light

--- tutorial-01/fragment_core.glsl
+++ tutorial-01/fragment_core.glsl
@@ -21,10 +21,9 @@ void main()
     vec3 ambientLight = vec3(0.1f, 0.1f, 0.1f);
 
     // Diffuse light
-    vec3 posToLightDirVec = normalize(vs_position - lightPos0);
-    vec3 diffuseColor = vec3(1.f, 1.f, 1.f);
-    float diffuse = clamp(dot(posToLightDirVec, vs_normal), 0, 1);
-    vec3 diffuseFinal = diffuseColor * diffuse;
+    vec3 lightDir = normalize(vs_position - lightPos0);
+    float diffuseIntensity = max(dot(vs_normal, lightDir), 0);
+    vec4 diffuseColor = vec4(1.f, 1.f, 1.f, 1.f) * diffuseIntensity;
 
-    fs_color *= texture(texture0, vs_texcoord) * (vec4(ambientLight, 1.f) + vec4(diffuseFinal, 1.f));
+    fs_color *= texture(texture0, vs_texcoord) * (vec4(ambientLight, 1.f) + diffuseColor);
 }
--- tutorial-01/main.cpp
+++ tutorial-01/main.cpp
@@ -24,7 +24,7 @@ struct Vertex
 };
 
 Vertex vertices[] = {
-    // Position                     // Color                            // Texcoords
+    // Position                     // Color                // Texcoords        // Normal
     glm::vec3(-0.5f, 0.5f, 0.f), glm::vec3(1.f, 0.f, 0.f), glm::vec2(0.f, 1.f), glm::vec3(0.f, 0.f, -1.f),
     glm::vec3(-0.5f, -0.5f, 0.f), glm::vec3(0.f, 1.f, 0.f), glm::vec2(0.f, 0.f), glm::vec3(0.f, 0.f, -1.f),
     glm::vec3(0.5f, -0.5f, 0.f), glm::vec3(0.f, 0.f, 1.f), glm::vec2(1.f, 0.f), glm::vec3(0.f, 0.f, -1.f),
